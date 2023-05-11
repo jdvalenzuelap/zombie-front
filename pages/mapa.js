ed1eceff37e5067fcecccb6fe99aff926e3c3084
Juan Diego Valenzuela <jd_valenzuelap@hotmail.com>
2023-05-10 18:30:22 -0600

    feat: informacion del punto

--- pages/mapa.js
+++ pages/mapa.js
@@ -1,12 +1,28 @@
-import MapComponent from "../components/MapComponent";
+import MapComponent from '../components/MapComponent'
+import { useState } from 'react'
 
 const Mapa = () => {
+  const [showResource, setShowResource] = useState(false)
+
   return (
     <div className="Mapa">
-      <h1 style={{display: "flex", justifyContent: "center"}}>Mapa de recursos</h1>
-      <MapComponent />
+      <h1 style={{ display: 'flex', justifyContent: 'center' }}>
+        Mapa de recursos
+      </h1>
+      <button onClick={() => setShowResource(prev => !prev)}>
+        recurso
+      </button>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <MapComponent />
+        {showResource && (
+          <div style={{ marginLeft: '2rem' }}>
+            <h2>Nombre del recurso</h2>
+            <p>Descripción del recurso</p>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
 
-export default Mapa;
+export default Mapa

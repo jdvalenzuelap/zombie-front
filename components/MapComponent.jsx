f4127ac818cd69045901d661523a1b4fe7aaf73d
AlfredoGodinez <78834172+AlfredoGodinez@users.noreply.github.com>
2023-06-06 21:43:01 -0600

    Added form and point fixes

--- components/MapComponent.jsx
+++ components/MapComponent.jsx
@@ -1,59 +1,56 @@
-import { useState } from "react";
-import "mapbox-gl/dist/mapbox-gl.css";
+import { useState } from 'react'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 import Map, {
-    Marker,
-    NavigationControl,
-    FullscreenControl,
-    GeolocateControl,
-  } from "react-map-gl";
+  Marker,
+  NavigationControl,
+  FullscreenControl,
+  GeolocateControl,
+} from 'react-map-gl'
 
-import config from "../config/keys";
+import config from '../config/keys'
 
 function MapComponent() {
-
   const [markers, setMarkers] = useState([
     { longitude: -103.348953, latitude: 20.659698 },
     { longitude: -103.448953, latitude: 20.759698 },
     { longitude: -103.548953, latitude: 20.859698 },
-  ]);
+  ])
 
   const addMarker = (longitude, latitude) => {
-    const newMarker = { longitude, latitude };
-    setMarkers([...markers, newMarker]);
-  };
+    const newMarker = { longitude, latitude }
+    setMarkers([...markers, newMarker])
+  }
+
+  const [showResource, setShowResource] = useState(false)
 
   const markerClick = (marker) => {
-    console.log("Hola")
-  };
+    setShowResource((prevValue) => !prevValue)
+  }
 
   return (
-    
-    <div className="Mapa" style={{display: "flex", justifyContent: "center"}} >
-      
+    <div className="Mapa" style={{ display: 'flex', justifyContent: 'center' }}>
       <Map
         mapboxAccessToken={config.mapBoxKey}
-        
         style={{
-            display: "flex",
-            width: "500px",
-            height: "500px",
-            borderRadius: "15px",
-            border: "2px solid red",
+          display: 'flex',
+          width: '500px',
+          height: '500px',
+          borderRadius: '15px',
+          border: '2px solid red',
         }}
-
         // latitude={markers[0].latitude}
         // longitude={markers[0].longitude}
         // zoom={8}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        
         {markers.map((marker, index) => (
-          <Marker 
+          <Marker
             key={index}
             longitude={marker.longitude}
             latitude={marker.latitude}
-            onClick={markerClick}/>
+            onClick={markerClick}
+          />
         ))}
 
         <NavigationControl position="bottom-right" />
@@ -61,8 +58,14 @@ function MapComponent() {
 
         <GeolocateControl />
       </Map>
+      {showResource && (
+        <div style={{ marginLeft: '2rem' }}>
+          <h2>Nombre del recurso</h2>
+          <p>Descripción del recurso</p>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default MapComponent;
+export default MapComponent
--- pages/mapa.js
+++ pages/mapa.js
@@ -3,21 +3,93 @@ import { useState } from 'react'
 
 const Mapa = () => {
   const [showResource, setShowResource] = useState(false)
+  const [showForm, setShowForm] = useState(false)
+
+  //Variables
+  const [nombreRecurso, setNombreRecurso] = useState('')
+  const [descripcionRecurso, setDescripcionRecurso] = useState('')
+  const [longitud, setLongitud] = useState(0)
+  const [latitud, setLatitud] = useState(0)
+
+  const handleGuardar = () => {
+    // Lógica para guardar la información ingresada en el formulario
+    setShowForm(false)
+  }
 
   return (
     <div className="Mapa">
-      <h1 style={{ display: 'flex', justifyContent: 'center' }}>
-        Mapa de recursos
-      </h1>
-      <button onClick={() => setShowResource(prev => !prev)}>
-        recurso
-      </button>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: '1rem',
+        }}
+      >
+        <h1 style={{ margin: 0, marginRight: '4rem' }}>Mapa de recursos</h1>
+        <button onClick={() => setShowForm(true)}>+</button>
+      </div>
+
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <MapComponent />
-        {showResource && (
-          <div style={{ marginLeft: '2rem' }}>
-            <h2>Nombre del recurso</h2>
-            <p>Descripción del recurso</p>
+        {showForm && (
+          <div className="modal">
+            <h2
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '1rem',
+              }}
+            >
+              Ingrese un Recurso
+            </h2>
+            <ul>
+              <li style={{ marginBottom: '0.5rem' }}>
+                <label htmlFor="nombre">Nombre del recurso:</label>
+                <input
+                  type="text"
+                  id="nombre"
+                  //value={nombreRecurso}
+                  //onChange={(e) => setNombreRecurso(e.target.value)}
+                />
+              </li>
+              <li style={{ marginBottom: '0.5rem' }}>
+                <label htmlFor="descripcion">Descripción del recurso:</label>
+                <textarea
+                  id="descripcion"
+                  //value={descripcionRecurso}
+                  //onChange={(e) => setDescripcionRecurso(e.target.value)}
+                ></textarea>
+              </li>
+              <li style={{ marginBottom: '0.5rem' }}>
+                <label htmlFor="longitud">Longitud:</label>
+                <input
+                  type="number"
+                  id="longitud"
+                  //value={longitud}
+                  //onChange={(e) => setLongitud(e.target.value)}
+                />
+              </li>
+              <li style={{ marginBottom: '0.5rem' }}>
+                <label htmlFor="latitud">Latitud:</label>
+                <input
+                  type="number"
+                  id="latitud"
+                  //value={latitud}
+                  //onChange={(e) => setLatitud(e.target.value)}
+                />
+              </li>
+            </ul>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '1rem',
+              }}
+            >
+              <button style={{ marginRight: '0.5rem' }} onClick={handleGuardar} >Guardar</button>
+              <button onClick={() => setShowForm(false)}>Cancelar</button>
+            </div>
           </div>
         )}
       </div>
